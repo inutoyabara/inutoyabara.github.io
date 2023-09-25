4c215412bf4b41967d7e8ef7a32832c4778e9b9d
inutoyabara <123281642+inutoyabara@users.noreply.github.com>
2023-09-25 22:15:14 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ $('#login-button').click(function (event) {
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
-			requestFullScreen();
+			//requestFullScreen();
 			setTimeout(function(){
 				location.href="BirthdayCake.html";
 			},2000);
